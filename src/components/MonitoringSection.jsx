ddf764d4add9ca77182c5efe3a9ac10d598c5708
AsifZaman777 <eramasif16@gmail.com>
2025-02-18 12:09:27 +0600

    socket monitoring is working well

--- src/components/MonitoringSection.jsx
+++ src/components/MonitoringSection.jsx
@@ -22,56 +22,106 @@ ChartJS.register(
   Legend
 );
 
-const stockSymbols = ["AAPL", "GOOGL", "TSLA", "MSFT", "AMZN"];
-
 const MonitoringSection = () => {
   const [logCounts, setLogCounts] = useState([]);
+  const [sendLogCounts, setSendLogCounts] = useState([]);
   const [labels, setLabels] = useState([]);
   const [logs, setLogs] = useState([]);
+  const [ws, setWs] = useState(null);
+  const [messageCount, setMessageCount] = useState(0);
+  const [sendMessageCount, setSendMessageCount] = useState(0);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://192.168.100.48:8001");
+
+    socket.onopen = () => {
+      console.log("WebSocket connected");
+      updateLogs("Connection established");
+    };
+
+    socket.onmessage = (e) => {
+      console.log("Message received:", e.data);
+      setMessageCount((prev) => prev + 1);
+      updateLogs(e.data);
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket disconnected");
+      updateLogs("Connection closed");
+    };
+
+    socket.onerror = (err) => {
+      console.error("WebSocket error:", err);
+    };
+
+    setWs(socket);
+
+    return () => {
+      socket.close();
+    };
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const currentTime = new Date().toLocaleTimeString();
-      const logCount = Math.floor(Math.random() * 5) + 1; // Random log count per second (1 to 5)
-
-      let newLogs = [];
-      for (let i = 0; i < logCount; i++) {
-        const randomStock =
-          stockSymbols[Math.floor(Math.random() * stockSymbols.length)];
-        const randomPrice = (Math.random() * 1000).toFixed(2);
-        newLogs.push({
-          symbol: randomStock,
-          price: `$${randomPrice}`,
-          timestamp: currentTime,
-        });
-      }
-
-      // Update graph data
+      
       setLogCounts((prev) =>
-        prev.length >= 10 ? [...prev.slice(1), logCount] : [...prev, logCount]
+        prev.length >= 20 ? [...prev.slice(1), messageCount] : [...prev, messageCount]
+      );
+      setSendLogCounts((prev) =>
+        prev.length >= 20 ? [...prev.slice(1), sendMessageCount] : [...prev, sendMessageCount]
       );
       setLabels((prev) =>
-        prev.length >= 10
-          ? [...prev.slice(1), currentTime]
-          : [...prev, currentTime]
+        prev.length >= 20 ? [...prev.slice(1), currentTime] : [...prev, currentTime]
       );
 
-      // Update logs
-      setLogs((prev) => [...prev, ...newLogs]);
+      setMessageCount(0);
+      setSendMessageCount(0);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [messageCount, sendMessageCount]);
+
+  const updateLogs = (data) => {
+    const currentTime = new Date().toLocaleTimeString();
+    setLogs((prev) => [...prev, { time: currentTime, message: data }]);
+  };
+
+  const sendSession = () => {
+    console.log("mt:LG");
+
+    const message =
+      '{"data":{"16":"mt010","37":4325,"271":"505ae561111b6bfd3fad9f3badb0d8ca200eefaf1dfbb2310f58d6c710acbbba","64":196608,"65":3},"mt":"LG"}';
+    ws.send(message);
+    setSendMessageCount((prev) => prev + 1);
+    setInterval(sendAck, 5000);
+  };
+
+  const sendAck = () => {
+    if (ws) {
+      const ackMessage = '{"mt":"AC","data":{}}';
+      ws.send(ackMessage);
+      setSendMessageCount((prev) => prev + 1);
+      updateLogs("Sent: " + ackMessage);
+    }
+  };
 
   const chartData = {
     labels,
     datasets: [
       {
-        label: "Logs Per Second",
+        label: "Received Messages Per Second",
         data: logCounts,
-        borderColor: "rgb(34, 197, 94)", // Green-200
+        borderColor: "rgb(34, 197, 94)",
         backgroundColor: "rgba(34, 197, 94, 0.2)",
-        tension: 0.5,
+        tension: 0.3,
+      },
+      {
+        label: "Send Messages Per Second",
+        data: sendLogCounts,
+        borderColor: "rgb(0, 123, 255)",
+        backgroundColor: "rgba(0, 123, 255, 0.2)",
+        tension: 0.2,
       },
     ],
   };
@@ -84,39 +134,47 @@ const MonitoringSection = () => {
     },
     scales: {
       x: { grid: { display: false } },
-      y: { beginAtZero: true, ticks: { stepSize: 1, maxTicksLimit: 5 } }, // Zoomed out
+      y: {
+        beginAtZero: true,
+        ticks: { stepSize: 1, maxTicksLimit: 10 },
+      },
     },
   };
 
   return (
     <div className="p-4 m-10 border-2 border-green-200 rounded-md shadow-md">
-      <h2 className="text-lg font-semibold text-green-300 border-b-1 mb-5">
+      <h2 className="text-md font-semibold text-green-300 border-b-1 mb-5">
         Socket Monitoring
       </h2>
 
-      <h2 className="text-sm font-semibold text-green-100">
-        Incoming data monitor
-      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {/* Graph Section */}
-        <div className="p-4 border-1 border-green-200 rounded-md shadow-md">
+        <div className="p-4 border border-green-200 rounded-md shadow-md">
           <Line data={chartData} options={chartOptions} />
         </div>
 
         {/* Logs Section */}
-        <div className="p-4 border-1 border-green-200 rounded-md shadow-md">
+        <div className="p-4 border border-green-200 rounded-md shadow-md">
           <h4 className="text-sm font-semibold text-green-300 mb-2">
-            Logs (Random Stock Prices)
+            Live Logs
           </h4>
           <div className="max-h-96 overflow-y-auto border border-gray-200 p-2 rounded-md bg-gray-900 text-green-300 text-xs">
             {logs.map((log, index) => (
-              <div key={index}>{JSON.stringify(log, null, 2)}</div>
+              <div key={index}>
+                <span className="font-bold text-blue-400">{log.time}:</span> {log.message}
+              </div>
             ))}
           </div>
         </div>
       </div>
 
-      
+      {/* Send Session Button */}
+      <button
+        className="mt-4 px-4 py-2 text-xs bg-green-500 hover:bg-green-600 text-white rounded"
+        onClick={sendSession}
+      >
+        Send Session
+      </button>
     </div>
   );
 };
--- src/components/ParamSection.jsx
+++ src/components/ParamSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import { VscDebugDisconnect } from "react-icons/vsc";
+import { GiNetworkBars } from "react-icons/gi";
 
 const ParamSection = () => {
   const [socketIP, setSocketIP] = useState("");
@@ -8,94 +9,80 @@ const ParamSection = () => {
   const [ackDelay, setAckDelay] = useState("");
   const [isConnected, setIsConnected] = useState(false);
 
-  const handleConnect = () => {
-    // Handle connect to socket logic
-  };
-
-  const handleSendAck = () => {
-    // Handle send ack logic
-  };
-
   return (
     <div className="p-2 m-20 border-2 border-green-200 rounded-md shadow-md mt-5">
-      <div className="flex items-center">
-        <h2 className="text-lg font-semibold text-green-300 mb-2">
-          Socket Connection
-        </h2>
-      </div>
-
+      <h2 className="text-md font-semibold text-green-300 border-b-1 mb-5">
+        Socket Connection
+      </h2>
       <div className="grid grid-cols-4 gap-2">
         <div>
-          <label className="text-sm">Socket IP</label>
+          <label className="text-xs">Socket IP</label>
           <input
             type="text"
             value={socketIP}
             onChange={(e) => setSocketIP(e.target.value)}
-            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-sm focus-visible:outline-none"
+            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-xs focus-visible:outline-none"
             placeholder="e.g ws://192.168.120.28"
           />
         </div>
         <div>
-          <label className="text-sm">Port</label>
+          <label className="text-xs">Port</label>
           <input
             type="text"
             value={port}
             onChange={(e) => setPort(e.target.value)}
-            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-sm focus-visible:outline-none"
+            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-xs focus-visible:outline-none"
             placeholder="e.g 8080"
           />
         </div>
         <div>
-          <label className="text-sm">Ack Delay(ms)</label>
+          <label className="text-xs">Ack Delay(ms)</label>
           <input
             type="text"
             value={ackDelay}
             onChange={(e) => setAckDelay(e.target.value)}
-            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-sm focus-visible:outline-none"
+            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-xs focus-visible:outline-none"
             placeholder="e.g 1000"
           />
         </div>
         <div>
-          <label className="text-sm">No of Threads (Users)</label>
+          <label className="text-xs">No of Threads (Users)</label>
           <input
             type="text"
             value={threads}
             onChange={(e) => setThreads(e.target.value)}
-            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-sm focus-visible:outline-none"
+            className="border p-2 rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-xs focus-visible:outline-none"
             placeholder="e.g 10"
+          />
+        </div>
+        <div>
+          <label className="text-xs">Request connection message</label>
+          <input
+            type="text"
+            value={threads}
+            onChange={(e) => setThreads(e.target.value)}
+            className="border p-2  rounded w-full hover:border-green-200 transition-all duration-150 text-green-300 text-xs focus-visible:outline-none"
+            placeholder={`{"data":{"16":"mt010","37":4325},"mt":"LG"}`}
           />
         </div>
       </div>
       <div className="mt-5">
-        <button
-          onClick={handleConnect}
-          className="bg-blue-500 text-white p-2 rounded mr-1 hover:bg-blue-600 transition-all duration-200 text-sm"
-        >
-          Connect to the socket
-        </button>
-        <button
-          onClick={handleSendAck}
-          className="bg-green-500 text-white p-2 rounded hover:bg-green-600 transition-all duration-200 text-sm"
-        >
-          Send ack
+        <button className="bg-blue-500 text-white p-2 rounded mr-1 hover:bg-blue-600 transition-all duration-200 text-xs">
+          Done
         </button>
       </div>
-      <div className="border-3 border-amber-300 mt-5"></div>
-      <div className="mt-5 flex items-center">
+
+      <div className="border-t-2 border-amber-200 mb-2 mt-2">
         {isConnected ? (
-          <>
-            <FaCheckCircle className="text-green-500 w-6 h-6 mr-2" />
-            <span className="text-lg font-semibold text-green-300">
-              Status: Connected
-            </span>
-          </>
+          <div className="flex items-center text-green-300 text-sm mt-2">
+            <GiNetworkBars className="mr-1" />
+            Connected to the socket
+          </div>
         ) : (
-          <>
-            <FaTimesCircle className="text-red-500 w-6 h-6 mr-2" />
-            <span className="text-lg font-semibold text-red-300">
-              Status: Disconnected
-            </span>
-          </>
+          <div className="flex items-center text-red-300 text-sm mt-2">
+            <VscDebugDisconnect className="mr-1" />
+            Not connected to the socket
+          </div>
         )}
       </div>
     </div>
